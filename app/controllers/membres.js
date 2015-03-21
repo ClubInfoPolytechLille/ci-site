bc6e4c536447aeaae4c3a0f593eb77a507730388
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-21 14:50:03 +0100

    Affichage des noms dans la liste des membres

--- app/controllers/membres.js
+++ app/controllers/membres.js
@@ -1,9 +1,25 @@
 var Membre = require('../models/membre');
+var noms = require('../controllers/noms');
+var async = require('async')
 
 var membres = {}
 
 membres.list = function (cb) {
-    Membre.find(cb);
+    Membre.find({}).lean().exec(function (err, membres) {
+        addNom = function (membre, cb) {
+            noms.get(membre.login, function (nom) {
+                if (nom) {
+                    membre.nom = nom
+                } else {
+                    membre.nom = membre.login
+                }
+                cb(null, membre)
+            })
+        }
+        async.mapSeries(membres, addNom, function (err, results) {
+            cb(results)
+        })
+    });
 }
 
 membres.add = function (data, cb) {
