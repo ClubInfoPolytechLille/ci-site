d03db7d029ba6d88c3a64f5c82b21e9a7670b6c2
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-04-04 12:53:36 +0200

    Ajout d'animations de changement de page

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('ciApp', ['ngRoute', 'SessionsCtrl', 'ConnectCtrl', 'MembreCtrl']).config(['$routeProvider', '$locationProvider',
+angular.module('ciApp', ['ngRoute', 'ngAnimate', 'SessionsCtrl', 'ConnectCtrl', 'MembreCtrl']).config(['$routeProvider', '$locationProvider',
     function ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
