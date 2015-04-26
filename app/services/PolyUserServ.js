9bd5849b5df436a34e10b8b4399786e778c10d55
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-04-26 13:12:13 +0200

    Amelioré PolyUserServ
    Le line-reader bugguait lors de lectures en parallèles. La nouvelle méthode
    utilise plus de mémoire, mais fonctionne dans les cas les plus extrêmes.

--- app/services/PolyUserServ.js
+++ app/services/PolyUserServ.js
@@ -1,5 +1,5 @@
-var LineTransform = require('node-line-reader').LineTransform;
 var fs = require('fs');
+var async = require('async');
 var Client = require('ssh2').Client;
 var creds = require('../../config/sshAuth');
 var NodeCache = require("node-cache");
@@ -12,17 +12,18 @@ var PolyUserServ = {};
 
 PolyUserServ.readPasswd = function (login, cb) {
     passwdF = 'config/passwd';
-    fs.exists(passwdF, function (exists) {
-        found = false;
-        if (exists) {
-            stream = fs.createReadStream(passwdF);
-            transform = new LineTransform();
-            stream.pipe(transform);
-            transform.on('data', function (line) {
+    fs.readFile(passwdF, function (err, file) {
+        if (err) {
+            cb(err);
+        } else {
+            lines = file.toString('utf8').split('\n');
+            async.detect(lines, function (line, cba) {
                 ex = line.split(':');
-                if (ex[0] == login) { // Si trouvé
-                    stream.close();
-                    cb({
+                cba(ex[0] == login);
+            }, function (res) {
+                if (res) {
+                    ex = res.split(':');
+                    cb(null, {
                         'username': ex[0],
                         'password': ex[1],
                         'UID': ex[2],
@@ -31,94 +32,75 @@ PolyUserServ.readPasswd = function (login, cb) {
                         'home': ex[5],
                         'shell': ex[6]
                     });
-                    found = true;
+                } else {
+                    cb(null, null);
                 }
             });
-            transform.on('end', function () {
-                if (!found) {
-                    cb(false);
-                }
-            });
-        } else {
-            cb(undefined);
         }
     });
 };
 
 PolyUserServ.readGroup = function (gid, cb) {
     groupF = 'config/group';
-    fs.exists(groupF, function (exists) {
-        found = false;
-        if (exists) {
-            stream = fs.createReadStream(groupF);
-            transform = new LineTransform();
-            stream.pipe(transform);
-            transform.on('data', function (line) {
+    fs.readFile(groupF, function (err, file) {
+        if (err) {
+            cb(err);
+        } else {
+            lines = file.toString('utf8').split('\n');
+            async.detect(lines, function (line, cba) {
                 ex = line.split(':');
-                if (ex[2] == gid) { // Si trouvé
-                    stream.close();
-                    cb({
+                cba(ex[2] == gid);
+            }, function (res) {
+                if (res) {
+                    ex = res.split(':');
+                    cb(null, {
                         'name': ex[0],
                         'password': ex[1],
                         'GID': ex[2],
                         'list': ex[3]
                     });
-                    found = true;
+                } else {
+                    cb(null, null);
                 }
             });
-            transform.on('end', function () {
-                if (!found) {
-                    cb(false);
-                }
-            });
-        } else {
-            cb(undefined);
         }
     });
 };
 
 PolyUserServ.grabInfos = function (login, cb) {
-    PolyUserServ.readPasswd(login, function (passwd) {
-        if (passwd) {
-            PolyUserServ.readGroup(passwd.GID, function (group) {
-                if (group) {
-                    cb({
-                        nom: passwd.GECOS,
-                        section: group.name.toUpperCase()
-                    });
-                } else {
-                    if (group === undefined) {
-                        console.error("Impossible d'ouvrir le fichier des groupes.");
-                    } else {
-                        console.error("Impossible d'obtenir le groupe de " + passwd.GID + ".");
-                    }
-                    cb({
-                        nom: passwd.GECOS,
-                        section: passwd.GID
-                    });
-                }
-            });
-        } else {
-            if (passwd === undefined) {
-                console.error("Impossible d'ouvrir le fichier des noms.");
+    async.waterfall([
+        function (cba) {
+            PolyUserServ.readPasswd(login, cba);
+        },
+        function (passwd, cba) {
+            if (passwd && passwd.GID) {
+                PolyUserServ.readGroup(passwd.GID, function (err, group) {
+                    cba(err, passwd, group);
+                });
             } else {
-                console.error("Impossible d'obtenir le nom de " + login + ".");
+                cba(null, passwd, null);
             }
-            if (!login) {
-                login = 'Inconnu';
-            }
-            cb({
-                nom: login.toUpperCase(),
-                section: 'Inconnue'
+        }
+    ], function (err, passwd, group) {
+        if (err) {
+            cb(err);
+        } else {
+            cb(null, {
+                nom: (passwd && passwd.GECOS) ? passwd.GECOS : login.toUpperCase(),
+                section: (group && group.name) ? group.name.toUpperCase() : ((passwd && passwd.GID) ? passwd.GID : 'Inconnu')
             });
         }
     });
 };
 
 PolyUserServ.add = function (login, cb) {
-    PolyUserServ.grabInfos(login, function (data) {
-        cb(null, data);
-        cache.set(login, data);
+    PolyUserServ.grabInfos(login, function (err, data) {
+        if (err) {
+            cb(err);
+        } else {
+            cb(null, data);
+            cache.set(login, data);
+        }
     });
 };
 
