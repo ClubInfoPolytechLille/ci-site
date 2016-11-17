1eb2cbefc5fb96d6b87d2c742eb3c4383cd5950c
Geoffrey Frogeye <geoffrey@frogeye.fr>
2016-11-17 21:39:01 +0100

    L'ordre s'il vous plait

--- app/services/NinfoServ.js
+++ app/services/NinfoServ.js
@@ -8,8 +8,8 @@ var NinfoServ = module.exports = {
         {id: 'nope', desc : "Je ne souhaite pas participer", ouvert: true, show: false},
         {id: 'ci', desc : "Équipe « Kilobits », pour se donner à fond", referent: ["Benoit Verhaeghe", "Benoit.Verhaeghe@polytech-lille.net"], ouvert: true, show: true},
         {id: 'nobrain', desc : "Équipe « 404 Brain Not Found », surtout pour passer un bon moment", referent: ["Cédric Roussel", "Cedric.Roussel@polytech-lille.net"], ouvert: true, show: true},
-        {id: 'gis5', desc : "Équipe GIS5", referent: ["Thibault Giordan", "Thibault.Giordan@polytech-lille.net"], ouvert: false, show: false},
         {id: 'peip', desc : "Équipe « PREPAration de l'IMAGISnation », des PeiP sérieux", referent: ["Romain Bailleul", "Romain.Bailleul@polytech-lille.net"], ouvert: true, show: true},
+        {id: 'gis5', desc : "Équipe GIS5", referent: ["Thibault Giordan", "Thibault.Giordan@polytech-lille.net"], ouvert: false, show: false},
         {id: 'other', desc : "Une autre équipe ?", referent: ["Geoffrey Preud'homme", "Geoffrey.Bontoux-Preud-Homme@polytech-lille.net"], ouvert: false, show: false},
     ],
 
